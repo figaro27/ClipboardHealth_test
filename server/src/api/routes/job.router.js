a26915edfc2c9373ba3e3f1814ee51d2f21d0ada
figaro27 <sweetfigaro27@gmail.com>
2020-11-21 15:26:51 +0800

    remove server

--- server/src/api/routes/job.router.js
+++
@@ -1,8 +0,0 @@
-import JobController from '../controllers/job.controller';
-
-const router = require("express").Router();
-
-router.get("/", JobController.index);
-router.get("/filter", JobController.getFilter);
-
-export default router;

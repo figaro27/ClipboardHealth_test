993b2fc44baabf6ab2bb74974ff0d543d87ffdba
figaro27 <sweetfigaro27@gmail.com>
2020-11-23 09:04:17 +0800

    change reduce

--- redux/actions/job_action.js
+++ redux/actions/job_action.js
@@ -10,28 +10,28 @@ axios.defaults.baseURL = ROOT_URL;
 
 export function getJobs() {
   return function (dispatch) {
-    axios
-      .get(`/jobs`)
-      .then(res => {
-          dispatch({type: JOB_LIST, payload: res.data})
-      })
-      .catch(error => {
-          console.log(error);
-      });
+    // axios
+    //   .get(`/jobs`)
+    //   .then(res => {
+    //       dispatch({type: JOB_LIST, payload: res.data})
+    //   })
+    //   .catch(error => {
+    //       console.log(error);
+    //   });
   }
 }
 
 export function getJobFilter() {
   return function (dispatch) {
-    axios
-      .get(`/jobs/filter`)
-      .then(res => {
-        console.log(res.data);
-          dispatch({type: JOB_FILTER, payload: res.data})
-      })
-      .catch(error => {
-          console.log(error);
-      });
+    // axios
+    //   .get(`/jobs/filter`)
+    //   .then(res => {
+    //     console.log(res.data);
+    //       dispatch({type: JOB_FILTER, payload: res.data})
+    //   })
+    //   .catch(error => {
+    //       console.log(error);
+    //   });
   }
 }
 
